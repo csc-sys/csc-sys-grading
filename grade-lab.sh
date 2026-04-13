a29ba56191305adfa2e86286f992b8db15237892
Jack Stillwell <jackwstillwell@gmail.com>
2026-04-13 17:27:12 -0500

    Tweaks for old oel server

--- grade-lab.sh
+++ grade-lab.sh
@@ -111,6 +111,7 @@ for f in "$@"; do
     touch tests/.agreement-accepted &> /dev/null
     # Some projects play with the username, checked using USER, so set it.
     export USER=${${f:t}/_*/_}
+    export GRADED_USER=$USER
     dotoutput timeout --foreground -k 8 $TIMEOUT $SANDBOX make test
     score=$(tail -n5 $logfile | sed -n '/.*FINAL.*/ { s@.*FINAL[^[:space:]]*[[:space:]]*\([0-9]*[[:space:]]*/[[:space:]]*[0-9]*\).*@\1@p;q }')
     echo "SCORE: $score"
